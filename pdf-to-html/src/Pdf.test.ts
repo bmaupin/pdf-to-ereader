b9f70c1a73ee8dc95da949c2aec82c23e9dee1f0
bmaupin <bmaupin@users.noreply.github.com>
2022-10-26 13:31:13 -0400

    test: Play around with pdf-lib

--- pdf-to-html/src/Pdf.test.ts
+++ pdf-to-html/src/Pdf.test.ts
@@ -1,10 +1,15 @@
 import { readFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
+
+import { inspect } from 'node:util';
+
 import { describe, expect, test } from 'vitest';
 
 import { Pdf } from './Pdf';
 
-describe('Pdf', () => {
+import { PDFDocument } from 'pdf-lib';
+
+describe.skip('Pdf', () => {
   test('Create new Pdf from ArrayBuffer', async () => {
     const arrayBuffer = (
       await readFile(resolve(__dirname, 'testdata/hello-world.pdf'))
@@ -13,3 +18,31 @@ describe('Pdf', () => {
     expect(pdf).toBeInstanceOf(Pdf);
   });
 });
+
+describe('pdf-lib', () => {
+  test('pdf-lib', async () => {
+    const arrayBuffer = (
+      await readFile(resolve(__dirname, 'testdata/hello-world.pdf'))
+    ).buffer;
+    const pdfDoc = await PDFDocument.load(arrayBuffer);
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+
+    console.log('firstPage=', inspect(firstPage));
+  });
+});
+
+/* Playing with pdf-lib in node REPL:
+const pdfLib = require('pdf-lib');
+const fs = require('fs');
+const path = require('path');
+
+let arrayBuffer = fs.readFileSync(path.resolve('src/testdata/hello-world.pdf')).buffer;
+let pdfDoc = await pdfLib.PDFDocument.load(arrayBuffer);
+const pages = pdfDoc.getPages();
+const firstPage = pages[0];
+*/
+
+/*
+firstPage.node is a map to pdf
+*/
--- pdf-to-html/wiki/research.md
+++ pdf-to-html/wiki/research.md
@@ -2,3 +2,8 @@
 
 - [PDF Succinctly](https://www.syncfusion.com/succinctly-free-ebooks/pdf)
 - Official PDF specs: [https://www.pdfa.org/resource/pdf-specification-index/](https://www.pdfa.org/resource/pdf-specification-index/)
+
+#### Libraries
+
+- [https://pdf-lib.js.org/](https://pdf-lib.js.org/)
+  - Too limited, no obvious way to get text
